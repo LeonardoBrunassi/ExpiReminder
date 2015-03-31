576bb6cfdf25bd5484b4b885447619dd70b1f0b6
Himalaiah <ahelaehil@hotmail.com>
2015-03-31 15:36:47 -0300

    Código Configurações

---
+++ ER/DataAlertaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DataAlertaTableViewCell.h
+//  ER
+//
+//  Created by André Helaehil on 31/03/15.
+//  Copyright (c) 2015 Vivian Chiodo Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataAlertaTableViewCell : UITableViewCell
+//@property (weak, nonatomic) 
+@end
