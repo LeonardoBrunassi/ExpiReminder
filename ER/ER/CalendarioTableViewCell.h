4c81d5a17c8a41a1c5a705a45f6667f7a1445349
LeonardoBrunassi <leo.brunassi.apple@hotmail.com>
2015-04-04 15:24:29 -0300

    no message

---
+++ ER/ER/CalendarioTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CalendarioTableViewCell.h
+//  ER
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 04/04/15.
+//  Copyright (c) 2015 Vivian Chiodo Dias. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarioTableViewCell : UITableViewCell
+
+@end
---
+++ ER/ER/CalendarioTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CalendarioTableViewCell.m
+//  ER
+//
+//  Created by Leonardo Rodrigues de Morais Brunassi on 04/04/15.
+//  Copyright (c) 2015 Vivian Chiodo Dias. All rights reserved.
+//
+
+#import "CalendarioTableViewCell.h"
+
+@implementation CalendarioTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
